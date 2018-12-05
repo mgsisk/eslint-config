9eb2f4d9af05f645bc5ed18ee701d707e397262f
Michael Sisk <me@mgsisk.com>
2018-12-05 11:11:36 -0500

    Add WordPress ESLint configuration

---
+++ wordpress.js
@@ -0,0 +1,333 @@
+module.exports = {
+	parserOptions: {
+		ecmaVersion: 10,
+		ecmaFeatures: {
+			globalReturn: false,
+			impliedStrict: true
+		},
+		sourceType: 'module'
+	},
+	env: {
+		browser: true,
+		node: true
+	},
+	plugins: [ 'eslint-comments', 'import', 'jsdoc', 'sonarjs' ],
+	extends: [ 'eslint:recommended', 'wordpress' ],
+	rules: {
+
+		// ----- Possible Errors ---------------------------------------------------
+
+		'no-async-promise-executor': 'error',
+		'no-await-in-loop': 'error',
+		'no-extra-parens': 'error',
+		'no-misleading-character-class': 'error',
+		'no-prototype-builtins': 'error',
+		'no-template-curly-in-string': 'error',
+		'require-atomic-updates': 'error',
+		'valid-jsdoc': [ 'error', {requireReturnDescription: false} ],
+
+		// ----- Best Practices ----------------------------------------------------
+
+		'accessor-pairs': 'error',
+		'array-callback-return': 'error',
+		'block-scoped-var': 'error',
+		'class-methods-use-this': 'error',
+		complexity: [ 'error', 8 ],
+		'default-case': 'error',
+		'dot-location': [ 'error', 'property' ],
+		eqeqeq: 'error',
+		'guard-for-in': 'error',
+		'max-classes-per-file': 'error',
+		'no-alert': 'error',
+		'no-caller': 'error',
+		'no-div-regex': 'error',
+		'no-else-return': 'error',
+		'no-empty-function': 'error',
+		'no-eq-null': 'error',
+		'no-eval': 'error',
+		'no-extend-native': 'error',
+		'no-extra-bind': 'error',
+		'no-extra-label': 'error',
+		'no-floating-decimal': 'error',
+		'no-implicit-coercion': 'error',
+		'no-implicit-globals': 'error',
+		'no-implied-eval': 'error',
+		'no-invalid-this': 'error',
+		'no-iterator': 'error',
+		'no-labels': 'error',
+		'no-lone-blocks': 'error',
+		'no-loop-func': 'error',
+		'no-multi-spaces': 'error',
+		'no-new': 'error',
+		'no-new-func': 'error',
+		'no-new-wrappers': 'error',
+		'no-octal-escape': 'error',
+		'no-proto': 'error',
+		'no-return-assign': [ 'error', 'always' ],
+		'no-return-await': 'error',
+		'no-script-url': 'error',
+		'no-self-compare': 'error',
+		'no-sequences': 'error',
+		'no-throw-literal': 'error',
+		'no-unmodified-loop-condition': 'error',
+		'no-unused-expressions': [
+			'error', {
+				allowShortCircuit: true,
+				allowTaggedTemplates: true
+			}
+		],
+		'no-useless-call': 'error',
+		'no-useless-concat': 'error',
+		'no-useless-return': 'error',
+		'no-void': 'error',
+		'no-warning-comments': 'error',
+		'prefer-promise-reject-errors': 'error',
+		radix: [ 'error', 'as-needed' ],
+		'require-await': 'error',
+		'require-unicode-regexp': 'error',
+
+		// ----- Variables ---------------------------------------------------------
+
+		'init-declarations': [ 'error', 'always' ],
+		'no-label-var': 'error',
+		'no-shadow': 'error',
+		'no-shadow-restricted-names': 'error',
+		'no-undef-init': 'error',
+		'no-undefined': 'error',
+		'no-use-before-define': [ 'error', 'nofunc' ],
+
+		// ----- Node.js and CommonJS ----------------------------------------------
+
+		'callback-return': 'error',
+		'handle-callback-err': 'error',
+		'no-buffer-constructor': 'error',
+		'no-mixed-requires': [ 'error', {allowCall: true} ],
+		'no-new-require': 'error',
+		'no-path-concat': 'error',
+		'no-process-env': 'error',
+		'no-process-exit': 'error',
+		'no-sync': 'error',
+
+		// ----- Stylistic Issues --------------------------------------------------
+
+		'array-bracket-newline': [ 'error', 'consistent' ],
+		'array-element-newline': [ 'error', 'consistent' ],
+		'block-spacing': [ 'error', 'never' ],
+		'capitalized-comments': 'error',
+		'computed-property-spacing': 'error',
+		'consistent-this': [ 'error', 'self' ],
+		'func-call-spacing': 'error',
+		'func-name-matching': 'error',
+		'func-names': [ 'error', 'as-needed' ],
+		'func-style': [ 'error', 'declaration' ],
+		'function-paren-newline': 'error',
+		'id-length': [ 'error', {exceptions: [ 'i', 'n', 'x' ]} ],
+		'implicit-arrow-linebreak': 'error',
+		indent: [ 'error', 'tab', {SwitchCase: 1} ],
+		'jsx-quotes': 'error',
+		'lines-between-class-members': 'error',
+		'max-depth': 'error',
+		'max-len': [
+			'error', {
+				code: 120,
+				tabWidth: 1,
+				ignoreRegExpLiterals: true,
+				ignoreTrailingComments: true,
+				ignoreTemplateLiterals: true,
+				ignoreUrls: true
+			}
+		],
+		'max-lines': [
+			'error', {
+				max: 1024,
+				skipBlankLines: true,
+				skipComments: true
+			}
+		],
+		'max-lines-per-function': [
+			'error', {
+				max: 128,
+				skipBlankLines: true,
+				skipComments: true
+			}
+		],
+		'max-nested-callbacks': 'error',
+		'max-params': [ 'error', {max: 8} ],
+		'max-statements': [ 'error', 16 ],
+		'max-statements-per-line': 'error',
+		'multiline-ternary': [ 'error', 'always-multiline' ],
+		'new-cap': 'error',
+		'new-parens': 'error',
+		'newline-per-chained-call': 'error',
+		'no-array-constructor': 'error',
+		'no-bitwise': 'error',
+		'no-lonely-if': 'error',
+		'no-multi-assign': 'error',
+		'no-negated-condition': 'error',
+		'no-nested-ternary': 'error',
+		'no-new-object': 'error',
+		'no-plusplus': [
+			'error', {
+				allowForLoopAfterthoughts: true
+			}
+		],
+		'no-restricted-syntax': 'error',
+		'no-ternary': 'error',
+		'no-underscore-dangle': 'error',
+		'no-unneeded-ternary': 'error',
+		'no-whitespace-before-property': 'error',
+		'nonblock-statement-body-position': 'error',
+		'object-curly-newline': [ 'error', {consistent: true} ],
+		'object-curly-spacing': 'error',
+		'object-property-newline': 'error',
+		'one-var': [
+			'error', {
+				initialized: 'never',
+				uninitialized: 'always'
+			}
+		],
+		'operator-assignment': 'error',
+		'padded-blocks': [ 'error', 'never' ],
+		'padding-line-between-statements': [
+			'error', {
+				blankLine: 'always',
+				prev: '*',
+				next: 'return'
+			}, {
+				blankLine: 'always',
+				prev: [ 'const', 'let', 'var' ],
+				next: '*'
+			}, {
+				blankLine: 'always',
+				prev: 'directive',
+				next: '*'
+			}, {
+				blankLine: 'any',
+				prev: [ 'const', 'let', 'var' ],
+				next: [ 'const', 'let', 'var' ]
+			}, {
+				blankLine: 'any',
+				prev: 'directive',
+				next: 'directive'
+			}
+		],
+		'prefer-object-spread': 'error',
+		'quote-props': [ 'error', 'as-needed' ],
+		'require-jsdoc': [
+			'error', {
+				require: {
+					FunctionDeclaration: true,
+					MethodDefinition: true,
+					ClassDeclaration: true
+				}
+			}
+		],
+		'semi-spacing': 'error',
+		'semi-style': 'error',
+		'spaced-comment': 'error',
+		'switch-colon-spacing': 'error',
+		'template-tag-spacing': 'error',
+		'unicode-bom': 'error',
+		'wrap-regex': 'error',
+
+		// ----- ECMAScript 6 ------------------------------------------------------
+
+		'arrow-body-style': 'error',
+		'arrow-parens': [ 'error', 'as-needed', {requireForBlockBody: true} ],
+		'arrow-spacing': [
+			'error', {
+				before: false,
+				after: true
+			}
+		],
+		'generator-star-spacing': 'error',
+		'no-confusing-arrow': 'error',
+		'no-duplicate-imports': 'error',
+		'no-useless-computed-key': 'error',
+		'no-useless-constructor': 'error',
+		'no-useless-rename': 'error',
+		'no-var': 'error',
+		'object-shorthand': 'error',
+		'prefer-arrow-callback': 'error',
+		'prefer-const': 'error',
+		'prefer-destructuring': 'error',
+		'prefer-numeric-literals': 'error',
+		'prefer-rest-params': 'error',
+		'prefer-spread': 'error',
+		'prefer-template': 'error',
+		'rest-spread-spacing': 'error',
+		'sort-imports': 'error',
+		'symbol-description': 'error',
+		'template-curly-spacing': 'error',
+		'yield-star-spacing': 'error',
+
+		// ----- Plugins -----------------------------------------------------------
+
+		'eslint-comments/no-duplicate-disable': 'error',
+		'eslint-comments/no-unlimited-disable': 'error',
+		'eslint-comments/no-unused-disable': 'error',
+		'eslint-comments/no-unused-enable': 'error',
+		'import/no-unresolved': [ 'error', {commonjs: true} ],
+		'import/named': 'error',
+		'import/default': 'error',
+		'import/namespace': 'error',
+		'import/no-absolute-path': 'error',
+		'import/no-dynamic-require': 'error',
+		'import/no-internal-modules': 'error',
+		'import/no-webpack-loader-syntax': 'error',
+		'import/no-self-import': 'error',
+		'import/no-cycle': 'error',
+		'import/no-useless-path-segments': 'error',
+		'import/no-relative-parent-imports': 'error',
+		'import/export': 'error',
+		'import/no-named-as-default': 'error',
+		'import/no-named-as-default-member': 'error',
+		'import/no-deprecated': 'error',
+		'import/no-extraneous-dependencies': 'error',
+		'import/no-mutable-exports': 'error',
+		'import/no-amd': 'error',
+		'import/first': 'error',
+		'import/exports-last': 'error',
+		'import/no-duplicates': 'error',
+		'import/no-namespace': 'error',
+		'import/extensions': [ 'error', 'always' ],
+		'import/newline-after-import': 'error',
+		'import/prefer-default-export': 'error',
+		'import/max-dependencies': [ 'error', {max: 16} ],
+		'import/no-unassigned-import': 'error',
+		'import/no-named-default': 'error',
+		'import/no-anonymous-default-export': 'error',
+		'import/group-exports': 'error',
+		'jsdoc/check-param-names': 'error',
+		'jsdoc/check-tag-names': 'error',
+		'jsdoc/check-types': 'error',
+		'jsdoc/newline-after-description': 'error',
+		'jsdoc/no-undefined-types': 'error',
+		'jsdoc/require-description-complete-sentence': 'error',
+		'jsdoc/require-param': 'error',
+		'jsdoc/require-param-description': 'error',
+		'jsdoc/require-param-name': 'error',
+		'jsdoc/require-param-type': 'error',
+		'jsdoc/require-returns-type': 'error',
+		'jsdoc/valid-types': 'error',
+		'sonarjs/no-all-duplicated-branches': 'error',
+		'sonarjs/no-element-overwrite': 'error',
+		'sonarjs/no-extra-arguments': 'error',
+		'sonarjs/no-identical-conditions': 'error',
+		'sonarjs/no-identical-expressions': 'error',
+		'sonarjs/no-use-of-empty-return-value': 'error',
+		'sonarjs/cognitive-complexity': [ 'error', 16 ],
+		'sonarjs/max-switch-cases': [ 'error', 8 ],
+		'sonarjs/no-duplicate-string': 'error',
+		'sonarjs/no-duplicated-branches': 'error',
+		'sonarjs/no-identical-functions': 'error',
+		'sonarjs/no-inverted-boolean-check': 'error',
+		'sonarjs/no-redundant-boolean': 'error',
+		'sonarjs/no-small-switch': 'error',
+		'sonarjs/no-useless-catch': 'error',
+		'sonarjs/prefer-immediate-return': 'error',
+		'sonarjs/prefer-object-literal': 'error',
+		'sonarjs/prefer-single-boolean-return': 'error',
+		'sonarjs/prefer-while': 'error'
+	}
+};
