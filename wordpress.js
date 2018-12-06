4b99adc936e36654edd6597d96532cb7bfdf67f6
Michael Sisk <me@mgsisk.com>
2018-12-06 13:46:55 -0500

    Fix formatting

--- wordpress.js
+++ wordpress.js
@@ -4,11 +4,10 @@
  * @type {Object}
  */
 module.exports = {
-	extends: [ './index.js', 'wordpress' ],
-	rules: {
-
-		// ----- Stylistic Issues --------------------------------------------------
-		indent: [ 'error', 'tab', {SwitchCase: 1} ],
-		'no-tabs': 'off'
-	}
-};
+  extends: ['./index.js', 'wordpress'],
+  rules: {
+    // ----- Stylistic Issues --------------------------------------------------
+    indent: ['error', 'tab', {SwitchCase: 1}],
+    'no-tabs': 'off',
+  },
+}
