e247cc2df61e26073217b12a1e17a2cb6e69d0ad
Michael Sisk <me@mgsisk.com>
2018-12-06 22:01:49 -0500

    Add same line exception for object-property-newline

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ module.exports = {
     'nonblock-statement-body-position': 'error',
     'object-curly-newline': ['error', {consistent: true}],
     'object-curly-spacing': 'error',
-    'object-property-newline': 'error',
+    'object-property-newline': ['error', {allowAllPropertiesOnSameLine: true}],
     'one-var': [
       'error', {
         initialized: 'never',
