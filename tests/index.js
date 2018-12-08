758f60cad981167018dd55d5ceffc84bfc38eddd
Michael Sisk <me@mgsisk.com>
2018-12-08 14:12:18 -0500

    Simplify test messages

--- tests/index.js
+++ tests/index.js
@@ -5,5 +5,5 @@ const engine = new eslint.CLIEngine({configFile: './index.js'})
 const [test1] = engine.executeOnText('const x = 1\n\nparseFloat(x)\n').results
 const [test2] = engine.executeOnText('let x = "y"').results
 
-tap.equal(test1.errorCount, 0, 'It finds no errors in a valid JavaScript file (standard)')
-tap.equal(test2.errorCount, 4, 'It finds errors in an invalid JavaScript file (standard)')
+tap.equal(test1.errorCount, 0, 'It passes valid JavaScript (standard)')
+tap.equal(test2.errorCount, 4, 'It lints invalid JavaScript (standard)')
--- tests/wordpress.js
+++ tests/wordpress.js
@@ -5,5 +5,5 @@ const engine = new eslint.CLIEngine({configFile: './wordpress.js'})
 const [test1] = engine.executeOnText('const x = 1;\n\nparseFloat( x );\n').results
 const [test2] = engine.executeOnText('var x = [1]').results
 
-tap.equal(test1.errorCount, 0, 'It finds no errors in a valid JavaScript file (wordpress)')
-tap.equal(test2.errorCount, 6, 'It finds errors in an invalid JavaScript file (wordpress)')
+tap.equal(test1.errorCount, 0, 'It passes a valid JavaScript (wordpress)')
+tap.equal(test2.errorCount, 6, 'It lints invalid JavaScript (wordpress)')
