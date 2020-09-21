8aa9abb0e5caf12bf05ced39d9064514c9681565
Michael Sisk <me@mgsisk.com>
2020-09-21 19:16:47 -0400

    Fix wordpress tests

--- test/wordpress.js
+++ test/wordpress.js
@@ -1,11 +1,9 @@
-'use strict'
-
 const eslint = require('eslint')
 const tap = require('tap')
 
 const engine = new eslint.CLIEngine({configFile: './wordpress.js'})
-const [test1] = engine.executeOnText('const x = 1;\n\nparseFloat(x);\n').results
+const [test1] = engine.executeOnText('const x = 1;\n\nparseFloat( x );\n').results
 const [test2] = engine.executeOnText('var x = [ 1 ]').results
 
-tap.equal(test1.errorCount, 0, 'It passes valid JavaScript (wordpress)')
-tap.equal(test2.errorCount, 3, 'It lints invalid JavaScript (wordpress)')
+tap.equal(test1.errorCount, 0, 'It passes valid JavaScript (WordPress)')
+tap.equal(test2.errorCount, 3, 'It lints invalid JavaScript (WordPress)')
