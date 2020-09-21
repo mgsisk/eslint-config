99efd7ff6d63dc3b1ebf2f750019c26087e5c301
Michael Sisk <me@mgsisk.com>
2020-09-21 19:16:58 -0400

    Relabel standard tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const eslint = require('eslint')
 const tap = require('tap')
 
@@ -7,5 +5,5 @@ const engine = new eslint.CLIEngine({configFile: './index.js'})
 const [test1] = engine.executeOnText('const x = 1\n\nparseFloat(x)\n').results
 const [test2] = engine.executeOnText('let x = "y"').results
 
-tap.equal(test1.errorCount, 0, 'It passes valid JavaScript (standard)')
-tap.equal(test2.errorCount, 4, 'It lints invalid JavaScript (standard)')
+tap.equal(test1.errorCount, 0, 'It passes valid JavaScript')
+tap.equal(test2.errorCount, 4, 'It lints invalid JavaScript')
