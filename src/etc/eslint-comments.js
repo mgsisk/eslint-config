6781d82498dbe995d11b44ddc7840f2e75946494
Michael Sisk <me@mgsisk.com>
2021-05-06 14:17:59 -0400

    Create eslint-comments.js

---
+++ src/etc/eslint-comments.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    'eslint-comments/no-unused-disable': 'error',
+    // 'eslint-comments/no-restricted-disable': 'error',
+    // 'eslint-comments/no-use': 'error',
+    // 'eslint-comments/require-description': 'error',
+  },
+}
