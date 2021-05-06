c92e9a55378d26ae80693e563927d5cf3a1443fa
Michael Sisk <me@mgsisk.com>
2021-05-06 14:18:50 -0400

    Update wordpress.js

--- test/wordpress.js
+++ test/wordpress.js
@@ -1,9 +1,18 @@
-const eslint = require('eslint')
+const {ESLint} = require('eslint')
 const tap = require('tap')
 
-const engine = new eslint.CLIEngine({configFile: './wordpress.js'})
-const [test1] = engine.executeOnText('const x = 1;\n\nparseFloat( x );\n').results
-const [test2] = engine.executeOnText('var x = [ 1 ]').results
+/**
+ * Run wordpress config tests.
+ *
+ * @returns {void}
+ */
+async function test() {
+  const eslint = new ESLint({overrideConfigFile: './src/cnf/wordpress.js', useEslintrc: false})
+  const [test1] = await eslint.lintText('const x = 1\n\nparseFloat( x )\n')
+  const [test2] = await eslint.lintText('var x = [ 1 ]')
 
-tap.equal(test1.errorCount, 0, 'It passes valid JavaScript (WordPress)')
-tap.equal(test2.errorCount, 3, 'It lints invalid JavaScript (WordPress)')
+  tap.equal(test1.errorCount, 0, 'It passes valid JavaScript (WordPress)')
+  tap.equal(test2.errorCount, 3, 'It lints invalid JavaScript (WordPress)')
+}
+
+test()
