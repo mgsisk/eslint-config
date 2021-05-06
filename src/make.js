1dd0ebeeda7fd36b3c12c55821e5c7d6159de005
Michael Sisk <me@mgsisk.com>
2021-05-06 14:18:31 -0400

    Create make.js

---
+++ src/make.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+fs.readdir(path.join(__dirname, 'cnf'), (readErr, files)=> {
+  if (readErr) {
+    throw new Error('Could not read cnf')
+  }
+
+  files.forEach(file=> fs.writeFile(
+    `./${file}`,
+    `const path=require('path');module.exports=require(path.join(__dirname,'src','cnf','${file}'))`,
+    (writeErr)=> {
+      if (writeErr) {
+        throw new Error(`Could not write ${file}`)
+      }
+    },
+  ))
+})
