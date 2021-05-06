144428f38a688b81d73f78121117945eb543a3c3
Michael Sisk <me@mgsisk.com>
2021-05-06 14:17:56 -0400

    Create index.js

---
+++ src/cnf/index.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/recommended',
+    'plugin:jsdoc/recommended',
+    'plugin:promise/recommended',
+    'plugin:sonarjs/recommended',
+    path.join(path.dirname(__dirname), 'etc', 'eslint'),
+    path.join(path.dirname(__dirname), 'etc', 'eslint-comments'),
+    path.join(path.dirname(__dirname), 'etc', 'import'),
+    path.join(path.dirname(__dirname), 'etc', 'jsdoc'),
+  ],
+}
