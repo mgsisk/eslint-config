0d50170ab03e1ca28130c813ef27307784a1477d
Michael Sisk <me@mgsisk.com>
2023-12-14 15:58:00 -0500

    Update babel.js

--- test/babel.js
+++ test/babel.js
@@ -1,9 +1,8 @@
-const {ESLint} = require('eslint')
-const tap = require('tap')
+const { ESLint } = require("eslint");
+const tap = require("tap");
 
 /**
  * Run babel config tests.
- *
  * @returns {void}
  */
 async function test() {
@@ -13,14 +12,14 @@ async function test() {
         requireConfigFile: false,
       },
     },
-    overrideConfigFile: './src/cnf/babel.js',
+    overrideConfigFile: "./src/cnf/babel.js",
     useEslintrc: false,
-  })
-  const [test1] = await eslint.lintText('const x = 1\n\nparseFloat(x)\n')
-  const [test2] = await eslint.lintText('let x = "y"')
+  });
+  const [testPass] = await eslint.lintText("const x = 1\n\nparseFloat(x)\n");
+  const [testFail] = await eslint.lintText('let x = "y"');
 
-  tap.equal(test1.errorCount, 0, 'It passes valid Babel')
-  tap.equal(test2.errorCount, 4, 'It lints invalid Babel')
+  tap.equal(testPass.errorCount, 0, "It passes valid Babel");
+  tap.equal(testFail.errorCount, 2, "It lints invalid Babel");
 }
 
-test()
+test();
