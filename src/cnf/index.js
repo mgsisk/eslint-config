836d5dfbcf557c6763f3688f2385c888fc9ea019
Michael Sisk <me@mgsisk.com>
2023-12-14 15:57:05 -0500

    Update index.js

--- src/cnf/index.js
+++ src/cnf/index.js
@@ -1,19 +1,19 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  env: {
-    es2021: true,
+  parserOptions: {
+    ecmaVersion: "latest",
   },
   extends: [
-    'eslint:recommended',
-    'plugin:eslint-comments/recommended',
-    'plugin:import/recommended',
-    'plugin:jsdoc/recommended',
-    'plugin:promise/recommended',
-    'plugin:sonarjs/recommended',
-    path.join(path.dirname(__dirname), 'etc', 'eslint'),
-    path.join(path.dirname(__dirname), 'etc', 'eslint-comments'),
-    path.join(path.dirname(__dirname), 'etc', 'import'),
-    path.join(path.dirname(__dirname), 'etc', 'jsdoc'),
+    "eslint:recommended",
+    "plugin:eslint-comments/recommended",
+    "plugin:import/recommended",
+    "plugin:jsdoc/recommended",
+    "plugin:promise/recommended",
+    "plugin:sonarjs/recommended",
+    path.join(path.dirname(__dirname), "etc", "eslint"),
+    path.join(path.dirname(__dirname), "etc", "eslint-comments"),
+    path.join(path.dirname(__dirname), "etc", "import"),
+    path.join(path.dirname(__dirname), "etc", "jsdoc"),
   ],
-}
+};
