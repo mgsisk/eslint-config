260f76d49be17ab70051668ae5d1b54552083270
Michael Sisk <me@mgsisk.com>
2023-12-14 15:57:55 -0500

    Update typescript.js

--- test/typescript.js
+++ test/typescript.js
@@ -1,27 +1,35 @@
-const {ESLint} = require('eslint')
-const path = require('path')
-const tap = require('tap')
+const { ESLint } = require("eslint");
+const path = require("path");
+const tap = require("tap");
 
 /**
  * Run typescript config tests.
- *
  * @returns {void}
  */
 async function test() {
   const eslint = new ESLint({
     baseConfig: {
       parserOptions: {
-        project: path.join(__dirname, 'typescript', 'tsconfig.json'),
+        project: path.join(__dirname, "typescript", "tsconfig.json"),
       },
     },
-    overrideConfigFile: path.join(path.dirname(__dirname), 'src', 'cnf', 'typescript.js'),
+    overrideConfigFile: path.join(
+      path.dirname(__dirname),
+      "src",
+      "cnf",
+      "typescript.js",
+    ),
     useEslintrc: false,
-  })
-  const [test1] = await eslint.lintFiles(path.join(__dirname, 'typescript', 'good.ts'))
-  const [test2] = await eslint.lintFiles(path.join(__dirname, 'typescript', 'bad.ts'))
+  });
+  const [testPass] = await eslint.lintFiles(
+    path.join(__dirname, "typescript", "good.ts"),
+  );
+  const [testFail] = await eslint.lintFiles(
+    path.join(__dirname, "typescript", "bad.ts"),
+  );
 
-  tap.equal(test1.errorCount, 0, 'It passes valid TypeScript')
-  tap.equal(test2.errorCount, 4, 'It lints invalid TypeScript')
+  tap.equal(testPass.errorCount, 0, "It passes valid TypeScript");
+  tap.equal(testFail.errorCount, 2, "It lints invalid TypeScript");
 }
 
-test()
+test();
